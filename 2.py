e78170dc428c2a115b0e31efb30ed5e5f09a5b15
dhinesh kumar <dhineshsurya0829@gmail.com>
2023-02-28 11:27:37 +0530

    cleared a problem

---
+++ 2.py
@@ -0,0 +1,11 @@
+#addtion of two number
+#subraction of two number
+#multiple of two number      
+
+
+
+a=int(input())
+b=int(input())
+print(a+b)
+print(a-b)
+print(a*b)
---
+++ 3.py
@@ -0,0 +1,13 @@
+#The provided code stub reads and integer,n, from STDIN. For all non-negative integers ,i<n print i square.
+#n=4
+#0
+#1
+#4
+#9
+
+
+
+n=int(input())
+for n in range(0,n):
+    i=n*n
+    print(i)
