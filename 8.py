d3bc617e58170a95b67ef00fc9876610b44ea5c3
dhinesh kumar <dhineshsurya0829@gmail.com>
2023-03-06 16:49:59 +0530

    AA

--- 8.py
+++ 8.py
@@ -3,9 +3,11 @@ y = int(input())
 z = int(input())
 n = int(input())
 # Using list comprehension
-newlist = [[i,j,k]
-for i in range(x+1) 
-for j in range(y+1)
-for k in range(z+1)
-    if (i+j+k) !=n]
-print(newlist)
+
+def list_comprehension(x,y,z,n):
+    newlist = [[i,j,k]
+    for i in range(x+1) 
+    for j in range(y+1)
+    for k in range(z+1)
+        if (i+j+k) !=n]
+    print(newlist)
--- 9.py
+++ 9.py
@@ -4,9 +4,9 @@ n3=int(input())
 n4=int(input())
 n5=int(input())
 n6=int(input())
-
-list1=(list(n1,n2,n3,n4,n5,n6))
-n=int(input())
-maximum=max(list1)-1
-if n==maximum:
-    print(maximum)
+def maxof(n1,n2,n3,n4,n5,n6):
+    list1=(list(n1,n2,n3,n4,n5,n6))
+    n=int(input())
+    maximum=max(list1)-1
+    if n==maximum:
+        print(maximum)
