7a5eef119e79b72cd71725302de47479fcf033fd
dhinesh kumar <dhineshsurya0829@gmail.com>
2023-03-10 12:34:21 +0530

    00

--- 9.py
+++ 9.py
@@ -1,49 +1,21 @@
-
+'''matrix_e=[[1,2,3],[4,5,6]]
+matrix_2=[]
+for arr in matrix_e:
+    arr2 =[]
+    for item in arr :
+        arr2.append(item+1)
+    matrix_2.append(arr2)
+print(matrix_2)
+ppp
 '''
-FIND THE RUNNER UP SCORE
-input format
-the first line contain n the second line contain an array A[] of n integers each separated by a space.
-
-constraints
-->2<=n<=10
-->-100<=a[i]<=100
-output format 
-print the runner up score
-
-sample input
-5
-2 3 6 6 5
-
-sample output
-5
-
-
-explaination
-given list is [2,3,6,6,5].that  maximum value of list is 6
-the runner is the logic minus of maximum then print the 
-value
-'''
-
-
-
-
-
-
-
-
-
-
-n1=int(input())
-n2=int(input())
-n3=int(input())
-n4=int(input())
-n5=int(input())
-n6=int(input())
-def maxof(n1,n2,n3,n4,n5,n6):
-    a=[]
-    list1=(list(n1,n2,n3,n4,n5,n6))
-    n=int(input())
-    maximum=max(list1)-1
-    return maximum
-maxof(n1,n2,n3,n4,n5,n6)
-
+n = int(input())
+def adding(n):
+    matrix = []
+    for i in range(1,n+1):
+        if i % 2 == 0:
+            matrix.append(i)
+        else:
+            pass
+    return matrix
+output = adding(n)
+print(output)
